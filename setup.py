7a3db86cffd270e4b3d528008f3e104b6d54527d
Indra Rudianto <indrarudianto.official@gmail.com>
2020-02-24 10:19:11 +0700

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+from meteo import __version__
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='bpptkg-meteo',
+    version=__version__,
+    description='BPPTKG weather station data fetcher library.',
+    long_description=read('README.md'),
+    long_description_file_content_type='text/markdown',
+    license='MIT',
+    install_requires=[
+        'sqlalchemy'
+    ],
+    author='Indra Rudianto',
+    author_email='indrarudianto.official@gmail.com',
+    zip_safe=False,
+    packages=find_packages(exclude=['docs', 'tests', 'examples']),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Operating System :: POSIX :: Linux',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
