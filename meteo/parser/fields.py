8136fe4a5cc194a987194c02061de275ff650bf1
Indra Rudianto <indrarudianto.official@gmail.com>
2021-04-26 13:24:02 +0700

    Add parser fields module

---
+++ meteo/parser/fields.py
@@ -0,0 +1,94 @@
+FIELDS_MAPPING = {
+    'Sn': {
+        'name': 'wind_speed_min',
+        'type': float
+    },
+    'Sm': {
+        'name': 'wind_speed_avg',
+        'type': float,
+    },
+    'Sx': {
+        'name': 'wind_speed_max',
+        'type': float,
+    },
+    'Dn': {
+        'name': 'wind_direction_min',
+        'type': float,
+    },
+    'Dm': {
+        'name': 'wind_direction_avg',
+        'type': float,
+    },
+    'Dx': {
+        'name': 'wind_direction_max',
+        'type': float,
+    },
+    'Pa': {
+        'name': 'air_pressure',
+        'type': float,
+    },
+    'Ta': {
+        'name': 'air_temperature',
+        'type': float,
+    },
+    'Tp': {
+        'name': 'internal_temperature',
+        'type': float,
+    },
+    'Ua': {
+        'name': 'relative_humidity',
+        'type': float,
+    },
+    'Rc': {
+        'name': 'rain_acc',
+        'type': float,
+    },
+    'Rd': {
+        'name': 'rain_duration',
+        'type': float,
+    },
+    'Ri': {
+        'name': 'rain_intensity',
+        'type': float,
+    },
+    'Rp': {
+        'name': 'rain_peak_intensity',
+        'type': float,
+    },
+    'Hc': {
+        'name': 'hail_acc',
+        'type': float,
+    },
+    'Hd': {
+        'name': 'hail_duration',
+        'type': float,
+    },
+    'Hi': {
+        'name': 'hail_intensity',
+        'type': float,
+    },
+    'Hp': {
+        'name': 'hail_peak_intensity',
+        'type': float,
+    },
+    'Th': {
+        'name': 'heating_temperature',
+        'type': float,
+    },
+    'Vh': {
+        'name': 'heating_voltage',
+        'type': float,
+    },
+    'Vs': {
+        'name': 'supply_voltage',
+        'type': float,
+    },
+    'Vr': {
+        'name': 'ref_voltage',
+        'type': float,
+    },
+    'Id': {
+        'name': 'id',
+        'type': str,
+    },
+}
