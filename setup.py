865b6da35bfdd9b4a9dce94085b8d41c73cc1712
Indra Rudianto <indrarudianto.official@gmail.com>
2022-02-16 15:43:40 +0700

    Remove python 3.5 support

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Intended Audience :: Science/Research',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
