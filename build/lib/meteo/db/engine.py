a6e2f2dbab9d7266f6c7e0830e65402ffc39366b
Indra Rudianto <indrarudianto.official@gmail.com>
2024-03-21 11:25:40 +0700

    Delete unused files and models

--- build/lib/meteo/db/engine.py
+++
@@ -1,5 +0,0 @@
-def prepare(base_class, engine):
-    """
-    Reflect base class to current database engine.
-    """
-    base_class.prepare(engine, reflect=True)
--- build/lib/meteo/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import cr6
-
-SCHEMAS = {
-    "cr6": {
-        "base": cr6.Base,
-        "CR6": cr6.CR6,
-    }
-}
